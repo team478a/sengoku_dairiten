6516b734450223c1b3e0971818cc2e880670ef8e
team478a <118187200+team478a@users.noreply.github.com>
2026-06-29 09:37:12 +0900

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "PHP動作確認: OK<br>";
-echo "PHPバージョン: " . PHP_VERSION . "<br>";
-echo "BASE_PATH: " . __DIR__ . "<br>";
-echo "config/存在: " . (is_dir(__DIR__ . '/config') ? 'YES' : 'NO') . "<br>";
-echo "config/書き込み: " . (is_writable(__DIR__ . '/config') ? 'YES' : 'NO') . "<br>";
-echo "PDO MySQL: " . (extension_loaded('pdo_mysql') ? 'YES' : 'NO') . "<br>";
-echo "cURL: " . (extension_loaded('curl') ? 'YES' : 'NO') . "<br>";
-echo "mbstring: " . (extension_loaded('mbstring') ? 'YES' : 'NO') . "<br>";
